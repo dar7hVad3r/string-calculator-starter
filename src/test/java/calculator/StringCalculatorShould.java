1d1faf03e0193d773704687efc3d2560f0ec2ff4
Sangram Barge <sangrambarge098@gmail.com>
2021-10-16 12:37:12 +0530

    common object calculator created, to ease up the object creation every time, code/test for empty string created

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -1,20 +1,21 @@
 package calculator;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalculatorShould {
-
-    @Test
-    void empty_string_should_return_0() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(0, stringCalculator.add(""));
+    StringCalculator calculator;
+    @BeforeEach
+    void beginTest(){
+        calculator = new StringCalculator();
     }
 
     @Test
-    void string_with_single_number_should_return_number_as_int() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(1, stringCalculator.add("1"));
+    void empty_string_should_return_0(){
+        assertEquals(0, calculator.add(""));
     }
+
+
 }
