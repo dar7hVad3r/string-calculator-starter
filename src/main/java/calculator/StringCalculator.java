be15eb9d4a428ad3ff337a37cb9267658633916c
Sangram Barge <sangrambarge098@gmail.com>
2021-10-16 20:16:33 +0530

    comments added, alternate solution refactored

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -17,44 +17,59 @@ class StringCalculator {
     }
 
     public int add(String input) {
+        // increment count each time method gets called
         this.addCalledCount += 1;
+
+        // check if string is empty
         if ( input.length() == 0 )
             return 0;
+
+        // StringBuilder objects delim and actInput for pattern matching and input filtration
         StringBuilder delim = new StringBuilder("\\n,");
         StringBuilder actInput = new StringBuilder();
+
+        // check if input needs to be filtered
         if ( input.startsWith("//") ){
-            delim.append(input, input.indexOf("//")+2, input.indexOf("\n"));
-            actInput.append( input.substring(input.indexOf("\n")) );
+            delim.append(input, input.indexOf("//")+2, input.indexOf("\n")); // string between '//' and '\n' is delmiter
+            actInput.append( input.substring(input.indexOf("\n")) ); // string after '\n' is input string
         }else{
             actInput.append(input);
         }
+
+        // flag to verify no negative numbers, negList to keep track of multiple negative values
         AtomicBoolean flag = new AtomicBoolean(false);
         List<String> negList = new ArrayList<>();
-//        int sum = 0;
-//        for ( String s : actInput.toString().split("["+delim+"]") ){
-//            if ( !s.isBlank() && Integer.parseInt(s) < 0 ){
-//                flag = true;
-//                negList.add(s+" ");
-//            } else if (!flag){
-//                sum += s.isBlank() ? 0 : Integer.parseInt(s);
-//            }
-//        }
-//        if (flag){
-//            throw new IllegalArgumentException("negatives not allowed "+negList);
-//        }
-//        return sum;
+/*
+
+        Alternate code which can be used instead of using stream()
+
+        int sum = 0;
+        for ( String s : actInput.toString().split("["+delim+"]") ){
+            if ( !s.isBlank() && Integer.parseInt(s) < 0 ){
+                flag.set(true);
+                negList.add(s+" ");
+            } else if (!flag.get()){
+                sum += s.isBlank() ? 0 : (Integer.parseInt(s)) > 1000 ? 0 : Integer.parseInt(s) ;
+            }
+        }
+        if (flag.get()){
+            throw new IllegalArgumentException("negatives not allowed "+negList);
+        }
+        return sum;
+*/
+        // iterate over each string that the delimiter has split
         int sum = Arrays.stream(actInput.toString().split( "["+delim+"]" ))
-                .filter(i-> !i.isBlank())
-                .mapToInt(Integer::parseInt)
-                .peek(i -> {
+                .filter(i-> !i.isBlank())   // filter if the string is blank
+                .mapToInt(Integer::parseInt)  // parse the integer value from string
+                .peek(i -> {                    // check if number is negative and set flag and list if it is
                     if ( i < 0 ) {
                         negList.add(String.valueOf(i));
                         flag.set(true);
                     }
                 })
-                .filter(i -> i<=1000)
-                .sum();
-        if ( flag.get() == true ) throw new IllegalArgumentException("negatives not allowed "+negList);
+                .filter(i -> i<=1000)   // filter if the number is greater than 1000
+                .sum();  // get addition of all the filtered values .reduce() can also be used
+        if ( flag.get()  ) throw new IllegalArgumentException("negatives not allowed "+negList);
         return sum;
     }
     public int GetCalledCount(){
